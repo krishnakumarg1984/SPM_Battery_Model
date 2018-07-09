2c7e0693543ecdbbb450b1db29584ce3012bb2c0
Krishnakumar <krishnakumar@imperial.ac.uk>
2018-07-09 21:08:19 +0100

    added post-process plots for spatio-temporal ce

--- main_ce_quadratic_simulation.m
+++ main_ce_quadratic_simulation.m
@@ -32,11 +32,11 @@ ce_sep_p2d_newconvention = fliplr(ce_sep_p2d);
 ce_pos_p2d_newconvention = fliplr(ce_pos_p2d);
 
 % cep2d_newconvention = [ce_neg_p2d_newconvention,ce_sep_p2d_newconvention,ce_pos_p2d_newconvention];
-
+return;
 %% Plot setup
 golden_ratio = 1.618;
 fig_width_factor = 1;
-fig_ht_factor = 0.7;
+fig_ht_factor = 0.6;
 ax_width_factor = 0.75;
 
 figW_cm = 15.74776*fig_width_factor; % textwidth (cm) reported by thesis latex doc from layouts package. Additonally include a scaling factor
@@ -45,20 +45,22 @@ figH_cm = figW_cm*golden_ratio*fig_ht_factor;
 n_axes_w = 2; % how many horizontal/width-wise axes?
 n_axes_ht = 3;  % how many vertical axes?
 
-marg_w_left = 1.25; % cm
-marg_w_right = 0.5; % cm
+marg_w_left = 1.75; % cm
+marg_w_right = 0.25; % cm
 marg_ht_top = 0.5; % cm
 marg_ht_bottom = 1.25; %cm
-gap_ht = 1; % cm
-gap_w = 1.25; % cm
+gap_ht = 1.25; % cm
+gap_w = 2.25; % cm
 
 run('setup_line_colors.m'); % deletes axes/clears plots
 
 %% Plot
 set(0,'defaultaxesfontsize',12 ...
     ,'defaultaxeslinewidth',2 ...
-    ,'defaultlinelinewidth',1 ...
+    ,'defaultlinelinewidth',2 ...
     ,'defaultpatchlinewidth',1);
+annot_xloc = 0.65;
+annot_yloc = 0.8;
 
 [ha, pos] = tight_subplot_cm(n_axes_ht, n_axes_w, [gap_ht gap_w],[marg_ht_bottom marg_ht_top],[marg_w_left marg_w_right],figH_cm,figW_cm);
 
@@ -77,7 +79,16 @@ plot(x_cell_p2d_plot_um,ce_p2d_tplot1,'color',line_colors(2,:));
 hold on;
 plot(x_cell_p2d_plot_um,ce_quadratic_tplot1,'color',line_colors(1,:));
 hold off;
-
+lgd = legend('P2d','Quadratic','location','southwest');
+legend boxoff;
+xticks(1e6*[0 param_p2d{1}.len_n param_p2d{1}.len_n+param_p2d{1}.len_s param_p2d{1}.len_n+param_p2d{1}.len_s+param_p2d{1}.len_p]);
+xticklabels({'0','$l_\mathrm{n}$','$l_\mathrm{n}\! + l_\mathrm{s}$','$l_\mathrm{tot}$'})
+xlim([0 x_cell_p2d_plot_um(end)]);
+line(1e6*[param_p2d{1}.len_n param_p2d{1}.len_n],get(gca,'YLim'),'linestyle','--','linewidth',0.5,'color','k');
+line(1e6*[param_p2d{1}.len_n+param_p2d{1}.len_s param_p2d{1}.len_n+param_p2d{1}.len_s],get(gca,'YLim'),'linestyle','--','linewidth',0.5,'color','k');
+lgd.String(3:end) = [];
+text(annot_xloc,annot_yloc,['t = ' num2str(time_vector_p2d(tplot1_idx)) ' s'],'Units','normalized');
+ylabel('$c_e\, (\mathrm{mol\, m}^{-3})$');
 
 axes(ha(2));
 ce_neg_tplot2_fittedObj = fit(z_neg_newconvention',ce_neg_p2d_newconvention(tplot2_idx,:)','smoothingspline');
@@ -94,7 +105,16 @@ plot(x_cell_p2d_plot_um,ce_p2d_tplot2,'color',line_colors(2,:));
 hold on;
 plot(x_cell_p2d_plot_um,ce_quadratic_tplot2,'color',line_colors(1,:));
 hold off;
-
+lgd = legend('P2d','Quadratic','location','southwest');
+legend boxoff;
+xticks(1e6*[0 param_p2d{1}.len_n param_p2d{1}.len_n+param_p2d{1}.len_s param_p2d{1}.len_n+param_p2d{1}.len_s+param_p2d{1}.len_p]);
+xticklabels({'0','$l_\mathrm{n}$','$l_\mathrm{n}\! + l_\mathrm{s}$','$l_\mathrm{tot}$'})
+xlim([0 x_cell_p2d_plot_um(end)]);
+line(1e6*[param_p2d{1}.len_n param_p2d{1}.len_n],get(gca,'YLim'),'linestyle','--','linewidth',0.5,'color','k');
+line(1e6*[param_p2d{1}.len_n+param_p2d{1}.len_s param_p2d{1}.len_n+param_p2d{1}.len_s],get(gca,'YLim'),'linestyle','--','linewidth',0.5,'color','k');
+lgd.String(3:end) = [];
+text(annot_xloc,annot_yloc,['t = ' num2str(time_vector_p2d(tplot2_idx)) ' s'],'Units','normalized');
+ylabel('$c_e\, (\mathrm{mol\, m}^{-3})$');
 
 axes(ha(3));
 ce_neg_tplot3_fittedObj = fit(z_neg_newconvention',ce_neg_p2d_newconvention(tplot3_idx,:)','smoothingspline');
@@ -111,7 +131,16 @@ plot(x_cell_p2d_plot_um,ce_p2d_tplot3,'color',line_colors(2,:));
 hold on;
 plot(x_cell_p2d_plot_um,ce_quadratic_tplot3,'color',line_colors(1,:));
 hold off;
-
+lgd = legend('P2d','Quadratic','location','southwest');
+legend boxoff;
+xticks(1e6*[0 param_p2d{1}.len_n param_p2d{1}.len_n+param_p2d{1}.len_s param_p2d{1}.len_n+param_p2d{1}.len_s+param_p2d{1}.len_p]);
+xticklabels({'0','$l_\mathrm{n}$','$l_\mathrm{n}\! + l_\mathrm{s}$','$l_\mathrm{tot}$'})
+xlim([0 x_cell_p2d_plot_um(end)]);
+line(1e6*[param_p2d{1}.len_n param_p2d{1}.len_n],get(gca,'YLim'),'linestyle','--','linewidth',0.5,'color','k');
+line(1e6*[param_p2d{1}.len_n+param_p2d{1}.len_s param_p2d{1}.len_n+param_p2d{1}.len_s],get(gca,'YLim'),'linestyle','--','linewidth',0.5,'color','k');
+lgd.String(3:end) = [];
+text(annot_xloc,annot_yloc,['t = ' num2str(time_vector_p2d(tplot3_idx)) ' s'],'Units','normalized');
+ylabel('$c_e\, (\mathrm{mol\, m}^{-3})$');
 
 axes(ha(4));
 ce_neg_tplot4_fittedObj = fit(z_neg_newconvention',ce_neg_p2d_newconvention(tplot4_idx,:)','smoothingspline');
@@ -128,7 +157,16 @@ plot(x_cell_p2d_plot_um,ce_p2d_tplot4,'color',line_colors(2,:));
 hold on;
 plot(x_cell_p2d_plot_um,ce_quadratic_tplot4,'color',line_colors(1,:));
 hold off;
-
+lgd = legend('P2d','Quadratic','location','southwest');
+legend boxoff;
+xticks(1e6*[0 param_p2d{1}.len_n param_p2d{1}.len_n+param_p2d{1}.len_s param_p2d{1}.len_n+param_p2d{1}.len_s+param_p2d{1}.len_p]);
+xticklabels({'0','$l_\mathrm{n}$','$l_\mathrm{n}\! + l_\mathrm{s}$','$l_\mathrm{tot}$'})
+xlim([0 x_cell_p2d_plot_um(end)]);
+line(1e6*[param_p2d{1}.len_n param_p2d{1}.len_n],get(gca,'YLim'),'linestyle','--','linewidth',0.5,'color','k');
+line(1e6*[param_p2d{1}.len_n+param_p2d{1}.len_s param_p2d{1}.len_n+param_p2d{1}.len_s],get(gca,'YLim'),'linestyle','--','linewidth',0.5,'color','k');
+lgd.String(3:end) = [];
+text(annot_xloc,annot_yloc,['t = ' num2str(time_vector_p2d(tplot4_idx)) ' s'],'Units','normalized');
+ylabel('$c_e\, (\mathrm{mol\, m}^{-3})$');
 
 axes(ha(5));
 ce_neg_tplot5_fittedObj = fit(z_neg_newconvention',ce_neg_p2d_newconvention(tplot5_idx,:)','smoothingspline');
@@ -145,7 +183,16 @@ plot(x_cell_p2d_plot_um,ce_p2d_tplot5,'color',line_colors(2,:));
 hold on;
 plot(x_cell_p2d_plot_um,ce_quadratic_tplot5,'color',line_colors(1,:));
 hold off;
-
+lgd = legend('P2d','Quadratic','location','southwest');
+legend boxoff;
+xticks(1e6*[0 param_p2d{1}.len_n param_p2d{1}.len_n+param_p2d{1}.len_s param_p2d{1}.len_n+param_p2d{1}.len_s+param_p2d{1}.len_p]);
+xticklabels({'0','$l_\mathrm{n}$','$l_\mathrm{n}\! + l_\mathrm{s}$','$l_\mathrm{tot}$'})
+xlim([0 x_cell_p2d_plot_um(end)]);
+line(1e6*[param_p2d{1}.len_n param_p2d{1}.len_n],get(gca,'YLim'),'linestyle','--','linewidth',0.5,'color','k');
+line(1e6*[param_p2d{1}.len_n+param_p2d{1}.len_s param_p2d{1}.len_n+param_p2d{1}.len_s],get(gca,'YLim'),'linestyle','--','linewidth',0.5,'color','k');
+lgd.String(3:end) = [];
+text(annot_xloc,annot_yloc,['t = ' num2str(time_vector_p2d(tplot5_idx)) ' s'],'Units','normalized');
+ylabel('$c_e\, (\mathrm{mol\, m}^{-3})$');
 
 axes(ha(6));
 ce_neg_tplot6_fittedObj = fit(z_neg_newconvention',ce_neg_p2d_newconvention(tplot6_idx,:)','smoothingspline');
@@ -162,3 +209,20 @@ plot(x_cell_p2d_plot_um,ce_p2d_tplot6,'color',line_colors(2,:));
 hold on;
 plot(x_cell_p2d_plot_um,ce_quadratic_tplot6,'color',line_colors(1,:));
 hold off;
+
+linkaxes(ha,'x');
+lgd = legend('P2d','Quadratic','location','southwest');
+legend boxoff;
+xticks(1e6*[0 param_p2d{1}.len_n param_p2d{1}.len_n+param_p2d{1}.len_s param_p2d{1}.len_n+param_p2d{1}.len_s+param_p2d{1}.len_p]);
+xticklabels({'0','$l_\mathrm{n}$','$l_\mathrm{n}\! + l_\mathrm{s}$','$l_\mathrm{tot}$'})
+xlim([0 x_cell_p2d_plot_um(end)]);
+line(1e6*[param_p2d{1}.len_n param_p2d{1}.len_n],get(gca,'YLim'),'linestyle','--','linewidth',0.5,'color','k');
+line(1e6*[param_p2d{1}.len_n+param_p2d{1}.len_s param_p2d{1}.len_n+param_p2d{1}.len_s],get(gca,'YLim'),'linestyle','--','linewidth',0.5,'color','k');
+lgd.String(3:end) = [];
+text(annot_xloc,annot_yloc,['t = ' num2str(time_vector_p2d(tplot6_idx)) ' s'],'Units','normalized');
+ylabel('$c_e\, (\mathrm{mol\, m}^{-3})$');
+
+
+cleanfigure;
+extra_axis_options = 'yticklabel style={/pgf/number format/1000 sep=, /pgf/number format/precision=2, /pgf/number format/fixed, }';
+custom_m2t_fcn('quadratic_ce_approx_spatial',[figW_cm,figH_cm]*10,[],false,extra_axis_options);
---
+++ post_process_thesis/quadratic_ce_temporal_plot_at_cc.m
@@ -0,0 +1,152 @@
+% Copyright (c) 2018 Gopalakrishnan, Krishnakumar <krishnak@vt.edu>
+% Author: Gopalakrishnan, Krishnakumar <krishnak@vt.edu>
+
+% clear;clc; format short g; format compact; close all;
+golden_ratio = 1.618;
+
+fig_width_factor = 1;
+ax_width_factor = 0.75;
+fig_ht_factor = 0.75;
+
+figW_cm = 15.74776*fig_width_factor;     % textwidth (cm) reported by LaTeX doc with a scaling factor
+figH_cm = figW_cm/golden_ratio;
+
+n_axes_w = 2; % how many horizontal/width-wise axes?
+n_axes_ht = 2;  % how many vertical axes?
+
+ax_width = (figW_cm/n_axes_w)*ax_width_factor;
+ax_height = ax_width/golden_ratio;
+
+% decision
+% left_margin : gap_w : right_margin
+gap_w_scale = 1;         % wrt to marg_w_left
+marg_w_right_scale = 0.3;   % wrt to marg_w_left
+
+% bottom_margin : gap_ht : top_margin
+gap_ht_scale = 0.9375;        % wrt to marg_ht_bottom
+marg_ht_top_scale = 0.3;     % wrt to marg_ht_bottom
+
+%%
+marg_w_left = (figW_cm - n_axes_w*ax_width)/(1 + gap_w_scale + marg_w_right_scale);
+gap_w = marg_w_left*gap_w_scale;
+marg_w_right = marg_w_left*marg_w_right_scale;
+
+marg_ht_bottom = (figH_cm - n_axes_ht*ax_height)/(1 + gap_ht_scale + marg_ht_top_scale);
+gap_ht = marg_ht_bottom*gap_ht_scale;
+marg_ht_top = marg_ht_bottom*marg_ht_top_scale;
+
+run('setup_line_colors.m'); % deletes axes/clears plots
+[ha, pos] = tight_subplot_cm(n_axes_ht, n_axes_w, [gap_ht gap_w],[marg_ht_bottom marg_ht_top],[marg_w_left marg_w_right],figH_cm,figW_cm);
+
+%% Post-process and plot
+no_of_x_tick_points = 5;
+no_of_y_tick_points = 5;
+t_end_max = max(quadratic_ce_sim_time_vector(end),time_vector_p2d(end));
+t_end_max_order = floor(log10(t_end_max));
+t_end_max_plot = ceil((t_end_max/10^t_end_max_order)/0.5)*0.5*10^t_end_max_order;
+
+
+clc;
+
+axes(ha(1));
+plot(time_vector_p2d,ce_neg_p2d_newconvention(:,1),'color',line_colors(2,:)); 
+hold on;
+plot(quadratic_ce_sim_time_vector,ce_neg_cc_quadratic,'color',line_colors(1,:));
+hold off;
+ylabel('$c_{\mathrm{e}_\mathrm{negcc}} (\mathrm{mol\, m}^{-3})$');
+lgd = legend('P2d','Quadratic','location','northwest');
+legend boxoff;
+xlim([0 t_end_max_plot]);
+ax_handle = gca;
+ax_handle.YAxis.TickValues = linspace(ax_handle.YAxis.Limits(1),ax_handle.YAxis.Limits(2),no_of_y_tick_points); % not for voltage
+ax_handle.XAxis.TickValues = linspace(ax_handle.XAxis.Limits(1),ax_handle.XAxis.Limits(2),no_of_x_tick_points);
+curtick = get(gca, 'XTick');
+set(gca, 'XTickLabel', cellstr(num2str(curtick(:)))); % remove scientific multipliers in x-axis format
+
+
+axes(ha(3));
+plot(time_vector_p2d,ce_pos_p2d_newconvention(:,end),'color',line_colors(2,:)); 
+hold on;
+plot(quadratic_ce_sim_time_vector,ce_pos_cc_quadratic,'color',line_colors(1,:));
+hold off;
+ylabel('$c_{\mathrm{e}_\mathrm{poscc}} (\mathrm{mol\, m}^{-3})$');
+lgd = legend('P2d','Quadratic','location','northwest');
+legend boxoff;
+xlim([0 t_end_max_plot]);
+ax_handle = gca;
+ax_handle.YAxis.TickValues = linspace(ax_handle.YAxis.Limits(1),ax_handle.YAxis.Limits(2),no_of_y_tick_points); % not for voltage
+ax_handle.XAxis.TickValues = linspace(ax_handle.XAxis.Limits(1),ax_handle.XAxis.Limits(2),no_of_x_tick_points);
+curtick = get(gca, 'XTick');
+set(gca, 'XTickLabel', cellstr(num2str(curtick(:)))); % remove scientific multipliers in x-axis format
+
+return;
+
+
+
+% plot(spm_sim_time_vector,v_cell_sim_results_spm,'color',line_colors(1,:),'linestyle','--');
+% hold off;
+% ylim([spm_params.CutoffVoltage spm_params.CutoverVoltage]);
+% ylabel('$V_\mathrm{cell}$ (V)');
+% lgd = legend('P2d','SPM','location','southeast');
+% lgd.Units = 'centimeters';
+% ax_handle = gca;
+% legend boxoff;
+% ytickformat('%.2f'); % for voltage
+% xlim([0 t_end_max_plot]);
+% ax_handle.YAxis.TickValues = linspace(ax_handle.YAxis.Limits(1),ax_handle.YAxis.Limits(2),no_of_y_tick_points); % not for voltage
+% ax_handle.XAxis.TickValues = linspace(ax_handle.XAxis.Limits(1),ax_handle.XAxis.Limits(2),no_of_x_tick_points);
+% curtick = get(gca, 'XTick');
+% set(gca, 'XTickLabel', cellstr(num2str(curtick(:)))); % remove scientific multipliers in x-axis format
+% 
+% axes(ha(2));
+% plot(time_vector_p2d,soc_pct_results_p2d,'color',line_colors(2,:),'linewidth',4);
+% hold on;
+% plot(spm_sim_time_vector,soc_pct_results_spm,'color',line_colors(1,:),'linestyle','--');
+% hold off;
+% ylim([0 100]); % for soc
+% ylabel('SOC (\%)');
+% lgd = legend('P2d','SPM','location','southeast');
+% lgd.Units = 'centimeters';
+% ax_handle = gca;
+% legend boxoff;
+% ytickformat('%.0f'); % for soc
+% xlim([0 t_end_max_plot]);
+% ax_handle.YAxis.TickValues = linspace(ax_handle.YAxis.Limits(1),ax_handle.YAxis.Limits(2),no_of_y_tick_points); % not for voltage
+% ax_handle.XAxis.TickValues = linspace(ax_handle.XAxis.Limits(1),ax_handle.XAxis.Limits(2),no_of_x_tick_points);
+% curtick = get(gca, 'XTick');
+% set(gca, 'XTickLabel', cellstr(num2str(curtick(:)))); % remove scientific multipliers in x-axis format
+% 
+% annotation_x = 0.50;
+% annotation_y = 0.55;
+% 
+% axes(ha(3));
+% abs_max_error_voltage_pct = max(abs(v_error_pct));
+% mae_voltage_pct = mean(abs(v_error_pct));
+% plot(t_common_vector,v_error_pct,'color',line_colors(1,:));
+% ylabel('$\hat{\varepsilon}_\mathrm{v} (\%)$');
+% ax_handle2 = gca;
+% ax_handle.YAxis.TickValues = linspace(ax_handle.YAxis.Limits(1),ax_handle.YAxis.Limits(2),no_of_y_tick_points); % not for voltage
+% ax_handle2.XAxis.TickValues = linspace(ax_handle2.XAxis.Limits(1),ax_handle2.XAxis.Limits(2),no_of_x_tick_points);
+% text(annotation_x,annotation_y,['$\%\, |\hat{\varepsilon}_v|_\mathrm{max}:\, $' num2str(abs_max_error_voltage_pct, '%4.2f')],'Units','normalized'); % for voltage
+% text(annotation_x,annotation_y-0.15,['$\%\, \hat{\varepsilon}_{v_\mathrm{mae}}\,\,\,\,\,:\, $' num2str(mae_voltage_pct, '%4.2f')],'Units','normalized'); % for voltage
+% 
+% xlabel('time (s)');
+% 
+% axes(ha(4));
+% abs_max_error_soc = max(abs(soc_error_pct));
+% mae_soc = mean(abs(soc_error_pct));
+% plot(t_common_vector,soc_error_pct,'color',line_colors(1,:));
+% ylabel('$\varepsilon_\mathrm{soc} (\%)$');
+% ax_handle2 = gca;
+% ax_handle.YAxis.TickValues = linspace(ax_handle.YAxis.Limits(1),ax_handle.YAxis.Limits(2),no_of_y_tick_points); % not for voltage
+% ax_handle2.XAxis.TickValues = linspace(ax_handle2.XAxis.Limits(1),ax_handle2.XAxis.Limits(2),no_of_x_tick_points);
+% text(annotation_x,annotation_y,['$\%\, |\hat{\varepsilon}_\mathrm{soc}|_\mathrm{max}:\, $' num2str(abs_max_error_soc, '%4.2f')],'Units','normalized'); % for voltage
+% text(annotation_x,annotation_y-0.15,['$\%\, \hat{\varepsilon}_{\mathrm{soc}_\mathrm{mae}}\,\,\,\,:\, $' num2str(mae_soc, '%4.2f')],'Units','normalized'); % for voltage
+% 
+% xlabel('time (s)');
+% 
+% %% Export to Tikz
+% extra_axis_options = 'xticklabel style={/pgf/number format/1000 sep=, /pgf/number format/precision=0,/pgf/number format/fixed,/pgf/number format/fixed zerofill,},yticklabel style={/pgf/number format/1000 sep=, /pgf/number format/precision=2, /pgf/number format/fixed, }, ylabel absolute,';
+% custom_m2t_fcn('const_curr_chg',[figW_cm,figH_cm]*10,[],false,extra_axis_options);
+% close;
+% % vim: set nospell nowrap textwidth=0 wrapmargin=0 formatoptions-=t:
